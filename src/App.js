86b13095e5089cfe6c94d8e5aeef182b4c40f3f0
lachicho <mcmoneta@gmail.com>
2021-04-01 19:09:06 -0300

    nivel 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import ScriptComponent from './ScriptComponent'
+import currentLine from './assets/data.json'
+
 
 function App() {
+  const scriptLines = currentLine.map(text => <ScriptComponent data={text} />)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          {scriptLines}            
+      </div>
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/ScriptComponent.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function ScriptComponent(props) {
+    return (
+        <div>
+            <h3>{props.data}</h3>
+            
+        </div>
+    )
+}
+
+export default ScriptComponent
+
+
+
